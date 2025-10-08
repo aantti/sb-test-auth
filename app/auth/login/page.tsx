f2d11e2179def7ac2e1c3db6cd2c73b33ca1ca1f
Andrey A. <56412611+aantti@users.noreply.github.com>
2025-10-08 17:45:16 +0200

    feat: add link to homepage

--- app/auth/login/page.tsx
+++ app/auth/login/page.tsx
@@ -1,10 +1,16 @@
 import { LoginForm } from "@/components/login-form";
+import Link from "next/link";
 
 export default function Page() {
   return (
     <div className="flex min-h-svh w-full items-center justify-center p-6 md:p-10">
       <div className="w-full max-w-sm">
         <LoginForm />
+          <div className="mt-4 text-center text-sm">
+            <Link href="/" className="underline underline-offset-4">
+            Return to homepage
+            </Link>
+          </div>
       </div>
     </div>
   );
